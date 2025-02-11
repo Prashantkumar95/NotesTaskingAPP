4d0713ef3da83b1c843ddc130b8d2b604f3c061d
Prashant Kumar <prashant63957174@gmail.com>
2025-02-11 17:02:17 +0530

    update

---
+++ Backend/Middlewares/Auth.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+
+const ensureAuthenticated = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if (!token) return res.status(401).json({ message: 'Unauthorized, JWT token is required' });
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch (err) {
+        return res.status(401).json({ message: 'Invalid JWT token' });
+    }
+};
+
+module.exports = ensureAuthenticated;
