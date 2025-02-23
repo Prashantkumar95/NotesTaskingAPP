2a55606e814de4174ab5253423b55194c52b35d4
Prashant Kumar <prashant63957174@gmail.com>
2025-02-23 16:21:03 +0530

    updating url

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -175,7 +175,7 @@ const Login = () => {
     }
 
     try {
-      const response = await fetch("http://localhost:8090/auth/login", {
+      const response = await fetch("https://ideavolt.onrender.com/auth/login", {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
