a83450024e09203ae70a64a3a4af22047f6b2736
Prashant Kumar <prashant63957174@gmail.com>
2025-03-01 17:08:40 +0530

    update

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,22 +1,22 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-export default defineConfig({
-  base: '/',
-  // base: '/NotesTaskingApp/',
-  plugins: [
-    tailwindcss(),
-  ],
-})
-
-
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react'; // Add this if you're using React
-// import tailwindcss from '@tailwindcss/vite';
-
+// import { defineConfig } from 'vite'
+// import tailwindcss from '@tailwindcss/vite'
 // export default defineConfig({
+//   base: '/',
+//   // base: '/NotesTaskingApp/',
 //   plugins: [
-//     react(), // Add this if you're using React
 //     tailwindcss(),
 //   ],
-// });
+// })
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react'; // Add this if you're using React
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+  plugins: [
+    react(), // Add this if you're using React
+    tailwindcss(),
+  ],
+});
 
