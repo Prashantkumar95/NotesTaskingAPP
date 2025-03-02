fc4aa69d9e04495c9d7701d9d511445ab4b3c9ee
Prashant Kumar <prashant63957174@gmail.com>
2025-03-02 11:06:57 +0530

    update

--- Backend/index.js
+++ Backend/index.js
@@ -29,7 +29,7 @@ const PORT = process.env.PORT || 8090;
 
 // CORS Configuration
 const allowedOrigins = [
-    'https://ideavolt-prashantkumar95s-projects.vercel.app/', // Your frontend URL
+    'https://ideavolt.vercel.app/', // Your frontend URL
     'http://localhost:3000', // Allow local development
     'https://ideavolt-hj4le72qi-prashantkumar95s-projects.vercel.app/',
 ];
